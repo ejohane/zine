ad23058d19680c9a493e3ca88513ec22e77aa5fe
Erik Johansson <ejohane@gmail.com>
2025-08-22 14:29:02 -0500

    fix

--- packages/api/migrations/0006_add_last_polled_at.sql
+++
@@ -1,6 +0,0 @@
--- Add lastPolledAt column to subscriptions table for optimized polling
-ALTER TABLE subscriptions ADD COLUMN last_polled_at INTEGER;
-
--- Create index for efficient polling queries
-CREATE INDEX idx_subscriptions_last_polled_at ON subscriptions(last_polled_at);
-CREATE INDEX idx_subscriptions_provider_last_polled ON subscriptions(provider_id, last_polled_at);
